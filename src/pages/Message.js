0e24ca09fd87eed96511d71d0a6e261750dbc760
Eason Liu <onpica17@gmail.com>
2020-03-31 04:04:02 +0800

    留言回覆系統

--- src/pages/Message.js
+++ src/pages/Message.js
@@ -27,12 +27,11 @@ const pageName = '訊息'
 
 const bgStyle = {
   flex: 1,
-  height: '300vh',
   padding: '0',
 }
 
 const Message = props => {
-  const [productID, setProductID] = useState('pt001')
+  const [productID, setProductID] = useState('2')
 
   useEffect(() => {
     // console.log('Message', props)
@@ -52,13 +51,13 @@ const Message = props => {
       <div id="page-wrapper" className="gray-bg" style={bgStyle}>
         <Header />
         <Banner pageName={pageName} />
-        <div className="wrapper wrapper-content">
+        <div className="wrapper wrapper-content" style={{ fontSize: '14px' }}>
           <div className="row animated fadeInRight">
             <div className="col-md-4">
               <div className="ibox ">
                 <div className="ibox-title">
                   {/* <!-- 左側的商品列表，預計未來改從資料庫讀取 --> */}
-                  <h5>商品列表</h5>
+                  <h2>商品列表</h2>
                 </div>
                 <div>
                   <div className="ibox-content no-padding border-left-right"></div>
@@ -69,8 +68,8 @@ const Message = props => {
                         return (
                           <ProductList
                             key={ind}
-                            pID={props.productListData[ind].pID}
-                            pName={props.productListData[ind].pName}
+                            pID={props.productListData[ind].itemId}
+                            pName={props.productListData[ind].itemName}
                             updateProductID={id => setProductID(id)}
                           />
                         )
@@ -89,12 +88,7 @@ const Message = props => {
             <div className="col-md-8">
               <div className="ibox">
                 <div className="ibox-title">
-                  <h5 id="product-comment-title">商品評論</h5>
-                  <div className="ibox-tools">
-                    <a className="collapse-link">
-                      <i className="fa fa-chevron-up"></i>
-                    </a>
-                  </div>
+                  <h2 id="product-comment-title">商品評論</h2>
                 </div>
                 <div className="ibox-content">
                   <div>
@@ -116,6 +110,9 @@ const Message = props => {
                                     props.userCommentData[ind].commentId
                                   ]
                                 }
+                                updateData={() => {
+                                  props.getReplyCommentData()
+                                }}
                               />
                             )
                           }
@@ -124,11 +121,6 @@ const Message = props => {
                   </div>
                 </div>
               </div>
-            </div>
-          </div>
-          <div className="footer">
-            <div>
-              <strong>Copyright</strong> Traveler3C Company &copy; 2020
             </div>
           </div>
         </div>
